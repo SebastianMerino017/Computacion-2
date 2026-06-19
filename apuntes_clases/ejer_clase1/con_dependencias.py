4300011b7985c4550e9deb00aab203e09fb40e8d
SebastianMerino017 <si.merino@alumno.um.edu.ar>
2026-06-18 22:33:46 -0300

    actualizo estructura

--- apuntes_clases/ejer_clase1/con_dependencias.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Script que usa una biblioteca externa."""
-
-import requests
-
-response = requests.get("https://httpbin.org/get")
-print(f"Status: {response.status_code}")
-print(f"Datos: {response.json()['origin']}")
